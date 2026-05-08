63076c5ca765b25d69b5dc612543da067dd64add
Jonghyo Lee <na1307@outlook.kr>
2026-05-08 20:48:39 +0900

    MSBee Project SDK

---
+++ Bluehill.MSBee.Sdk/FixAssembly.cs
@@ -0,0 +1,26 @@
+﻿using dnlib.DotNet;
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace MSBee.AssemblyFixer;
+
+public sealed class FixAssembly : Task {
+    [Required]
+    public required string TargetPath { get; init; }
+
+    public override bool Execute() {
+        var ad = AssemblyDef.Load(TargetPath);
+        var module = ad.ManifestModule;
+        module.Cor20HeaderRuntimeVersion = 0x20000;
+        module.TablesHeaderVersion = 0x100;
+
+        var originalPath = Path.Combine(Path.GetDirectoryName(TargetPath)!,
+            Path.GetFileNameWithoutExtension(TargetPath) + "-original" + Path.GetExtension(TargetPath));
+
+        File.Delete(originalPath);
+        File.Move(TargetPath, originalPath);
+        ad.Write(TargetPath);
+
+        return true;
+    }
+}
---
+++ Bluehill.MSBee.Sdk/README.md
@@ -0,0 +1,3 @@
+# MSBee Project SDK
+
+The MSBee project SDK allows you to build projects with .NET Framework 1.x. For more information, please refer to [the wiki](https://github.com/na1307/MSBee/wiki).
